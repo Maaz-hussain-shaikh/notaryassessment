decb7f518c4ed5098b851f85ae3ec987d984c5ba
Maaz <maazratlam5@gmail.com>
2022-10-29 10:38:03 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import logo from './logo.svg';
+import { React, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, updata] = useState([]);
+
+  const fetchdata = async () => {
+    const main = await fetch("http://demo2211087.mockable.io/mock", {
+      method: 'POST',
+      headers: {
+        'Accept': 'name',
+        'Content-Type': 'text/html',
+        'Path': 'mock',
+
+      },
+      body: {}
+    })
+    const json = await main.json();
+    updata(json.companies);
+  }
+  useEffect(() => {
+    fetchdata();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav">
+        <ul>
+          <div className='navdiv'>
+
+            <li>
+              <a href='/'>Client </a>
+            </li>
+          </div>
+          <div className='navdiv'>
+
+            <li>
+              <a href='/'>Orders </a>
+            </li>
+          </div>
+          <div className='navdiv'>
+
+            <li>
+              <a href='/'>Messages</a>
+            </li>
+          </div>
+          <div className='navdiv'>
+
+            <li>
+              <a href='/'>User</a>
+            </li>
+          </div>
+          <div className='navdiv'>
+
+            <li>
+              <a href='/'>settings</a>
+            </li>
+          </div>
+        </ul>
+      </nav>
+      <hr />
+
+
+      <div>
+        <div className='namelist'>
+          <div className="col-md-12">
+            <div className="table-wrap">
+              <table className="table table-responsive-xl">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Email</th>
+                    <th>Username</th>
+                    <th>Status</th>
+
+                    <th>&nbsp;</th>
+                  </tr>
+                </thead>
+
+
+
+
+                <tbody>
+
+                  {data.map((elm, index) => {
+                    
+                    return (<>
+                      <tr className="alert" role="alert">
+                        <td>
+                          <label className="checkbox-wrap checkbox-primary">
+                            <h3>{index}</h3>
+                            <span className="checkmark"></span>
+                          </label>
+                        </td>
+                        <td className="d-flex align-items-center">
+
+                          <div className="pl-3 email">
+                            <span>{data[index].email}</span>
+
+                          </div>
+                        </td>
+                        <td>{data[index].name}</td>
+                        <td className="status"><span className="active">{data[index].status}</span></td>
+                      </tr>
+                    </>);
+
+                  })}
+
+
+
+                </tbody>
+
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
